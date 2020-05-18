2682e8f83963882dc7d5f955b00137203f9bfc48
kverscharen <55915246+kverscharen@users.noreply.github.com>
2020-05-18 17:16:34 -0400

    Add files via upload

---
+++ TripleX.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "You are a mage building an array." << std::endl;
+    std::cout << "Enter the right number to continue..." << std::endl;
+
+    int a = 4;
+
+    std::cout << a;
+    return 0;
+    }
